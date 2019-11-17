f0e8cab1dab47b0f1f700c42690edd6819dac07a
pylsar <kysov2006@bk.ru>
2019-11-17 17:11:37 +0300

    router

--- my_instagram/src/components/Profile.js
+++ my_instagram/src/components/Profile.js
@@ -6,9 +6,9 @@ const Profile = () => {
     return (
         <div className="container profile">
             <User 
-                src={photo} 
-                alt={altname}
-                name={name} 
+                src='https://static.wixstatic.com/media/632d61_73f435284b574a2686e5f503b04135d0~mv2_d_2560_1707_s_2.jpg/v1/fill/w_1600,h_1066,al_c,q_90/file.jpg'
+                alt= 'picture'
+                name='horhe'
             />
             <Palette />
         </div>
